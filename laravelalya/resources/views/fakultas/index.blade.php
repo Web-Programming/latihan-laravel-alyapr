e7a7473992e1193027bef65c4132c4ffb1676bdc
VenEsuella <126873233+VenEsuella@users.noreply.github.com>
2024-05-08 13:08:31 +0700

    latihanlaravel

---
+++ laravelalya/resources/views/fakultas/index.blade.php
@@ -0,0 +1,10 @@
+{{-- @include('Layouts.header') --}}
+@extends ('layouts.master')
+
+{{-- @section('tittle', 'Universitas MDP') --}}
+@section('content')
+    <h1>Fakultas</h1>
+    <li>{{$Ilkom}}</li>
+
+{{-- @include('Layouts.footer') --}}
+@endsection
