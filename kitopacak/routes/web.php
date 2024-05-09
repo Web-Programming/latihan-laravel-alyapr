b7fc460901400166ebe3612f79bd58868dae62c9
alyapr <putririzaldyalya@gmail.com>
2024-05-09 13:52:18 +0700

    Persiapan Routes and Views

---
+++ kitopacak/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home');
+});
+
+Route::get('/About', function () {
+    return view('About');
+});
+
+Route::get('/Contact', function () {
+    return view('Contact');
+});
+
+Route::get('/Register', function () {
+    return view('Register');
+});
+
+Route::get('/Login', function(){
+    return view('Login');
+});
