46ed93910897eef4f2443199f0f386c41b016426
alyapr <putririzaldyalya@gmail.com>
2024-05-20 09:56:31 +0700

    Membuat Model

--- latihanLaravel/app/Http/Controllers/ProdiController.php
+++ latihanLaravel/app/Http/Controllers/ProdiController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prodi;
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
-    //
+    public function index(){
+        $listprodi = Prodi::all();
+        return view("prodi.index", ['listprodi' => $listprodi]);
+    }
+
+    public function show($id){
+        $prodi = Prodi::find($id);
+        return view("prodi.show", ['prodi' => $prodi]);
+    }
+
+    public function create(){
+        return view("prodi.create");
+    }
+
+    public function store(Request $request) {
+        //dump($request);
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20'
+        ]);
+
+        $prodi = new Prodi();
+        $prodi->nama = $validateData['nama'];
+        $prodi->save();
+
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+        return redirect()->route('prodi.create');
+    }
 }
