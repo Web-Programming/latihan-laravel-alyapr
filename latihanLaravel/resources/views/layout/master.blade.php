5eeb2cd55fcc9c2877f14ef5ae46bbaa274a8ee5
alyapr <putririzaldyalya@gmail.com>
2024-05-29 10:51:49 +0700

    edit Layout

--- latihanLaravel/resources/views/layout/master.blade.php
+++ latihanLaravel/resources/views/layout/master.blade.php
@@ -8,11 +8,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Universitas MDP</h1>
+    @if(isset($kampus)) <h1> {{ $kampus }} </h1> @endif
     <hr/>
+
     @yield('content')
+
     <hr/>
-    {{date("Y")}} &copy; Universitas MDP
+    {{ date('Y')}} Univessita Multi Data Palembang
+
 </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
