0d8e8368d7fd5128796cd588a8323c63483aa13a
alyapr <putririzaldyalya@gmail.com>
2024-05-29 10:54:34 +0700

    Update Views Prodi

--- latihanLaravel/resources/views/prodi/create.blade.php
+++ latihanLaravel/resources/views/prodi/create.blade.php
@@ -1,22 +1,29 @@
-@extends ('layouts.master')
-@section('content') 
+@extends('layouts.master')
+@section('content')
     <h1>Tambah Program Studi</h1>
     @if(session()->has('info'))
-        <div class="alert alert-sucess">
-            {{session()->get('info')}}
+        <div class="alert alert-success">
+        {{ session()->get('info') }}
         </div>
     @endif
-    <form method="POST" action="{{url('prodi/store')}}">
+    <form method="POST" action="{{ url('prodi/store')}}" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
-            <label> Nama Prodi</label>
-            <input name ="nama"
-            class="form-control" type="text"
-            required minlength="5" value="{{old('nama')}}" maxlength="20">
+            <label>Nama Prodi</label>
+            <input name="nama" class="form-control" type="text" required minlength="5" value="{{old('nama')}}" maxlength="20">
             @error('nama')
-                <div class="text-danger">
-                {{ $message }}</div>
+                <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
 
-        <button type="submit" class="btn btn-primary">
+        <div class="form-group">
+            <label for="nama">Foto/logo</label>
+            <input type="file" name="foto" id="foto" class="form-control">
+            @error('foto')
+                <div class="text-danger"> {{ $message }} </div>
+            @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+@endsection
--- latihanLaravel/resources/views/prodi/index.blade.php
+++ latihanLaravel/resources/views/prodi/index.blade.php
@@ -1,20 +1,48 @@
 @extends('layouts.master')
 @section('content')
     <h1>Program Studi</h1>
+     @if (session()->has('info'))
+        <div class="alert alert-success">
+        {{ session()->get('info') }}
+        </div>
+    @endif
+    <a href="{{route('prodi.create')}}" class="btn btn-success">
+        Tambah Prodi
+    </a>
     <table class="table table-stiped">
         <thead>
             <tr>
-                <th>NPM</th>
-                <th>Nama</th>
-                <th>Prodi</th>
+                <th>No</th>
+                <th>Nama Prodi</th>
+                <th>Foto</th>
+                <th>Aksi</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($allmahasiswaprodi as $item)
+            @foreach($prodis as $item)
                 <tr>
-                    <td>{{ $item->npm }}</td>
-                    <td>{{ $item->nama_mahasiswa }}</td>
-                    <td>{{ $item->nama_prodi }}</td>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $item->nama }}</td>
+                    <td>
+                        @if($item->foto != '')
+                            <img src="{{ asset('storage/'.$item->foto) }}" width="100"/>
+                        @endif
+                    </td>
+                    <td>
+                        <form action="{{ route('prodi.destroy', ['prodi' => $item->id]) }}" method="POST">
+                            @method("DELETE")
+                            @csrf
+                            <a href="{{ url('/prodi/'.$item->id) }}" class="btn btn-warning">
+                                Detail
+                            </a>
+                            <a href="{{ route('prodi.edit', [$item->id]) }}" class="btn btn-info">
+                                Edit
+                            </a>
+                            <button class="btn btn-danger" type="submit">
+                            Hapus
+                            </button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
